d1bcc60f70a6eab7265ec0ccc9d38f960addfb3d
joshbernsteint <joshuatbernstein@gmail.com>
2023-03-17 22:58:49 -0400

    trying to fix nav for hopefully the last time again again again

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./home"
 import { MyProjects } from './MyProjects'
 import { Stack, Nav, Navbar, Container, Col } from "react-bootstrap";
@@ -13,13 +13,13 @@ function MyNavBar() {
   <>
     <Navbar bg="black" variant="dark" fixed = "top" expand = "lg" className={`my-0`}>
       <Container fluid>
-        <Navbar.Brand href="/">Joshua Bernstein</Navbar.Brand>
+        <Navbar.Brand href="/#">Joshua Bernstein</Navbar.Brand>
         <Navbar.Toggle aria-controls = "navbarScroll"/>
         <Navbar.Collapse id = "navbarScroll" style = {{alignItems: "right",justifyContent:"right"}}>
         <Nav className={`my-1 my-lg-2 ${navStyles.nav}`}  navbarScroll>
-            <Nav.Link href="/" className={`${navStyles.navLink}`}>Home</Nav.Link>
-            <Nav.Link href="/projects" className={`${navStyles.navLink}`}>Projects</Nav.Link>
-            <Nav.Link href="/resume" className={`${navStyles.navLink}`}>Resumé</Nav.Link> 
+            <Nav.Link href="/#" className={`${navStyles.navLink}`}>Home</Nav.Link>
+            <Nav.Link href="/#projects" className={`${navStyles.navLink}`}>Projects</Nav.Link>
+            <Nav.Link href="/#resume" className={`${navStyles.navLink}`}>Resumé</Nav.Link> 
             <Nav.Link href="https://www.linkedin.com/in/joshua-bernstein-9700261b0/" className={`${navStyles.navLink}`} target="_blank" rel="noreferrer noopener">LinkedIn</Nav.Link>
             <Nav.Link href="https://github.com/joshbernsteint" className={`${navStyles.navLink}`} target="_blank" rel="noreferrer noopener">GitHub</Nav.Link>
           
@@ -78,10 +78,11 @@ function App() {
     <Stack gap={2} className={`${styles.base}`}>
       <h1>-----</h1>
       <MyNavBar />
-      <Switch>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/projects" element={<MyProjects/>} />
-            <Route path="/resume" element={
+      {/* <HashRouter>
+        <Routes>
+            <Route path="/#" element={<Home />} />
+            <Route path="/#projects" element={<MyProjects/>} />
+            <Route path="/#resume" element={
                 <embed
                 src="./joshuabernsteinresume-2.pdf"
                 type="application/pdf"
@@ -89,7 +90,9 @@ function App() {
                 height="100%"
                 />}/>
             <Route path="*" element={<Navigate to ="/#" />} />
-      </Switch>
+        </Routes>
+      </HashRouter> */}
+      <RouterProvider router={router}/>
       <Footer style={{height: "100px"}}/>
     </Stack >
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Home } from './home'
 import {
-  HashRouter
+  createHashRouter,
+  Router,
+  RouterProvider
 } from 'react-router-dom';
 import { MyProjects } from './MyProjects';
 
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <Home />,
+  },
+  {
+    path: "/projects",
+    element: <MyProjects/>,
+  },
+  {
+    path: "/resume",
+    element: <embed
+      src="./joshuabernsteinresume-2.pdf"
+      type="application/pdf"
+      width="100%"
+      height="100%"
+      />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App/>
-    </HashRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
